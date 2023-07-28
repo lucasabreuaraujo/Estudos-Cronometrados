dab0aeac693bf3ce82b77c58959f53940f6eeeda
Lucas <lucas.abreuaraujo10@gmail.com>
2023-07-28 15:07:08 -0300

    erro de svg sass somenta na pasta Lista

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.scss'
+
+class Botao extends React.Component {
+    render() {
+        return(
+            <button className="botao">
+                Botao
+            </button>
+        )
+    }
+}
+export default Botao;
---
+++ src/components/pages/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Formulario from '../Formulario';
+import Lista from '../Lista/Lista';
+import './style.scss'
+
+
+
+
+
+function App() {
+  return (
+    <div className="AppStyle">
+     <Formulario />
+     <Lista />
+    </div>
+  );
+}
+
+export default App;
